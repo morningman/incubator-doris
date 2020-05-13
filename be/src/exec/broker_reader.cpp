apache/doris

Support load json-data into Doris by RoutineLoad or StreamLoad

be/src/exec/broker_reader.cpp
@@ -121,6 +121,11 @@ Status BrokerReader::open() {
     return Status::OK();
 }
 
+//not support
+Status BrokerReader::read_all(uint8_t** buf, size_t *length) {
+    return Status::NotSupported("Not support");
+}
+
 Status BrokerReader::read(uint8_t* buf, size_t* buf_len, bool* eof) {
     DCHECK_NE(*buf_len, 0);
     RETURN_IF_ERROR(readat(_cur_offset, (int64_t)*buf_len, (int64_t*)buf_len, buf));

be/src/exec/local_file_reader.cpp
@@ -52,6 +52,25 @@ bool LocalFileReader::closed() {
     return _fp == nullptr;
 }
 
+// Read all bytes
+Status LocalFileReader::read_all(uint8_t** buf, size_t *length) {
+    bool eof;
+    int64_t file_size = size() - _current_offset;
+    if (file_size <= 0) {
+        *buf = nullptr;
+        *length = 0;
+        return Status::OK();
+    }
+    *length = file_size;
+    *buf = new uint8_t[file_size];
+    read(*buf, length, &eof);
+    if (*length == 0) {
+        delete *buf;
+        *buf = nullptr;
+    }
+    return Status::OK();
+}
+
 Status LocalFileReader::read(uint8_t* buf, size_t* buf_len, bool* eof) {
     readat(_current_offset, (int64_t)*buf_len, (int64_t*)buf_len, buf);
     if (*buf_len == 0) {
@@ -113,6 +132,5 @@ Status LocalFileReader::tell(int64_t* position) {
     *position = _current_offset;
     return Status::OK();
 }
-
 
 }

be/src/exec/local_file_reader.h
@@ -37,6 +37,7 @@ public:
     // is set to zero.
     virtual Status read(uint8_t* buf, size_t* buf_len, bool* eof) override;
     virtual Status readat(int64_t position, int64_t nbytes, int64_t* bytes_read, void* out) override;
+    virtual Status read_all(uint8_t** buf, size_t *length) override;
     virtual int64_t size () override;
     virtual Status seek(int64_t position) override;
     virtual Status tell(int64_t* position) override;

be/src/runtime/stream_load/stream_load_pipe.h
@@ -45,6 +45,13 @@ public:
         return Status::OK();
     }
 
+    Status append_and_flush(const char* data, size_t size) {
+        ByteBufferPtr buf = ByteBuffer::allocate(BitUtil::RoundUpToPowerOfTwo(size+1));
+        buf->put_bytes(data, size);
+        buf->flip();
+        return _append(buf);
+    }
+
     Status append(const char* data, size_t size) override {
         size_t pos = 0;
         if (_write_buf != nullptr) {
@@ -60,7 +67,7 @@ public:
                 _write_buf.reset();
             }
         }
-        // need to allocate a new chunk
+        // need to allocate a new chunk, min chunk is 64k
         size_t chunk_size = std::max(_min_chunk_size, size - pos);
         chunk_size = BitUtil::RoundUpToPowerOfTwo(chunk_size);
         _write_buf = ByteBuffer::allocate(chunk_size);
@@ -75,6 +82,33 @@ public:
             _write_buf.reset();
         }
         return _append(buf);
+    }
+
+    Status read_all(uint8_t** data, size_t *length) override {
+        std::unique_lock<std::mutex> l(_lock);
+        while (!_cancelled && !_finished && _buf_queue.empty()) {
+            _get_cond.wait(l);
+        }
+        // cancelled
+        if (_cancelled) {
+            return Status::InternalError("cancelled");
+        }
+        // finished
+        if (_buf_queue.empty()) {
+            DCHECK(_finished);
+            *data = nullptr;
+            *length = 0;
+            return Status::OK();
+        }
+        auto buf = _buf_queue.front();
+        *length = buf->remaining();
+        *data = new uint8_t[*length];
+        buf->get_bytes((char*)(*data) , *length);
+
+        _buf_queue.pop_front();
+        _buffered_bytes -= buf->limit;
+        _put_cond.notify_one();
+        return Status::OK();
     }
 
     Status read(uint8_t* data, size_t* data_size, bool* eof) override {
@@ -180,6 +214,7 @@ private:
         _get_cond.notify_one();
         return Status::OK();
     }
+
 
     // Blocking queue
     std::mutex _lock;

morningman/incubator-doris

be/src/exec/broker_reader.cpp
@@ -121,6 +121,11 @@ Status BrokerReader::open() {
     return Status::OK();
 }
 
+//not support
+Status BrokerReader::read_one_message(uint8_t** buf, size_t* length) {
+    return Status::NotSupported("Not support");
+}
+
 Status BrokerReader::read(uint8_t* buf, size_t* buf_len, bool* eof) {
     DCHECK_NE(*buf_len, 0);
     RETURN_IF_ERROR(readat(_cur_offset, (int64_t)*buf_len, (int64_t*)buf_len, buf));

be/src/exec/local_file_reader.cpp
@@ -52,6 +52,25 @@ bool LocalFileReader::closed() {
     return _fp == nullptr;
 }
 
+// Read all bytes
+Status LocalFileReader::read_one_message(uint8_t** buf, size_t* length) {
+    bool eof;
+    int64_t file_size = size() - _current_offset;
+    if (file_size <= 0) {
+        *buf = nullptr;
+        *length = 0;
+        return Status::OK();
+    }
+    *length = file_size;
+    *buf = new uint8_t[file_size];
+    read(*buf, length, &eof);
+    if (*length == 0) {
+        delete *buf;
+        *buf = nullptr;
+    }
+    return Status::OK();
+}
+
 Status LocalFileReader::read(uint8_t* buf, size_t* buf_len, bool* eof) {
     readat(_current_offset, (int64_t)*buf_len, (int64_t*)buf_len, buf);
     if (*buf_len == 0) {
@@ -113,6 +132,5 @@ Status LocalFileReader::tell(int64_t* position) {
     *position = _current_offset;
     return Status::OK();
 }
-
 
 }

be/src/exec/local_file_reader.h
@@ -37,6 +37,7 @@ public:
     // is set to zero.
     virtual Status read(uint8_t* buf, size_t* buf_len, bool* eof) override;
     virtual Status readat(int64_t position, int64_t nbytes, int64_t* bytes_read, void* out) override;
+    virtual Status read_one_message(uint8_t** buf, size_t* length) override;
     virtual int64_t size () override;
     virtual Status seek(int64_t position) override;
     virtual Status tell(int64_t* position) override;

be/src/runtime/stream_load/stream_load_pipe.h
@@ -45,6 +45,13 @@ public:
         return Status::OK();
     }
 
+    Status append_and_flush(const char* data, size_t size) {
+        ByteBufferPtr buf = ByteBuffer::allocate(BitUtil::RoundUpToPowerOfTwo(size+1));
+        buf->put_bytes(data, size);
+        buf->flip();
+        return _append(buf);
+    }
+
     Status append(const char* data, size_t size) override {
         size_t pos = 0;
         if (_write_buf != nullptr) {
@@ -60,7 +67,7 @@ public:
                 _write_buf.reset();
             }
         }
-        // need to allocate a new chunk
+        // need to allocate a new chunk, min chunk is 64k
         size_t chunk_size = std::max(_min_chunk_size, size - pos);
         chunk_size = BitUtil::RoundUpToPowerOfTwo(chunk_size);
         _write_buf = ByteBuffer::allocate(chunk_size);
@@ -75,6 +82,33 @@ public:
             _write_buf.reset();
         }
         return _append(buf);
+    }
+
+    Status read_one_message(uint8_t** data, size_t* length) override {
+        std::unique_lock<std::mutex> l(_lock);
+        while (!_cancelled && !_finished && _buf_queue.empty()) {
+            _get_cond.wait(l);
+        }
+        // cancelled
+        if (_cancelled) {
+            return Status::InternalError("cancelled");
+        }
+        // finished
+        if (_buf_queue.empty()) {
+            DCHECK(_finished);
+            *data = nullptr;
+            *length = 0;
+            return Status::OK();
+        }
+        auto buf = _buf_queue.front();
+        *length = buf->remaining();
+        *data = new uint8_t[*length];
+        buf->get_bytes((char*)(*data) , *length);
+
+        _buf_queue.pop_front();
+        _buffered_bytes -= buf->limit;
+        _put_cond.notify_one();
+        return Status::OK();
     }
 
     Status read(uint8_t* data, size_t* data_size, bool* eof) override {
@@ -180,6 +214,7 @@ private:
         _get_cond.notify_one();
         return Status::OK();
     }
+
 
     // Blocking queue
     std::mutex _lock;